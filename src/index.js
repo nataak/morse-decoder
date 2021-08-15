24a63d8539ec8e40de5ae1bd73a2e4b81be13363
Nata <nata.ak848@gmail.com>
2021-08-15 17:02:36 +0400

    feat: end task

--- src/index.js
+++ src/index.js
@@ -36,9 +36,24 @@ const MORSE_TABLE = {
     '----.':  '9',
     '-----':  '0',
 };
-
 function decode(expr) {
-    // write your solution here
+    let wordsArray = expr.split('**********');
+    let lettersArray = []
+    for (let item of wordsArray) {
+       let letters =  item.match(/.{1,10}/g);
+       lettersArray.push(letters);
+    }
+let expression = '';
+    for (let item of lettersArray) {
+       item = item.map(str => {
+        str = str.replace(/10/g, '.').replace(/11/g, '-').replace(/0/g, '');
+        return str.replace(str, MORSE_TABLE[str]);         
+        });
+
+        expression += item.join('') + ' ';
+
+    }
+    return expression.slice(0, -1);
 }
 
 module.exports = {
